5ed336a7fa51e223e7a90823fa1640ed6f465cdd
hyunmila <kamilaak90801@gmail.com>
2024-09-29 05:53:51 +0200

    emocje_siegnely_zenitu

--- emotions_detection/emotions_detection.py
+++ emotions_detection/emotions_detection.py
@@ -1,25 +1,25 @@
 import cv2
 from fer import FER
 import numpy as np
+import json
+import argparse
+import os
 
 
-def save_emotions_data(filename:str, data:dict):
-    with open(f'emotions_detection/{filename}_test.txt', 'w') as file:
-        file.write(str(data))
+parser = argparse.ArgumentParser(description='Trim start splash screen from video')
+parser.add_argument('--input', type=str, help='input video file path')
+parser.add_argument('--output', type=str, help='output json file path')
 
-def get_filename(input_file:str)->str:
-    return input_file.replace(".mp4","_data")
 
-def main(videos_path:str, file_path:str):
-    input_video = cv2.VideoCapture(videos_path+file_path)
+def main(file_path:str, json_path:str):
+    input_video = cv2.VideoCapture(file_path)
     frame_rate = input_video.get(cv2.CAP_PROP_FPS)
     frame_idx = 0
 
     emotion_detector = FER()
     emotions_dict = {'angry':[], 'disgust':[], 'fear':[], 'happy':[], 'sad':[], 'surprise':[], 'neutral':[]}
     emotions_labels = list(emotions_dict.keys())
-    # emotion_history = []
-    # threshold = 0.1
+
     previous_value = 0
     found_zboczes = 0
     max_emotions_dict = {}
@@ -38,51 +38,56 @@ def main(videos_path:str, file_path:str):
             if frame_idx>0:
                 previous_value = emotions_dict[str(emotion)][frame_idx-1]
             diff = percent - previous_value
-            # print(emotion, previous_value, percent)
             if diff>0.4 and emotion!='neutral':
-                # print(f"Frame {frame_idx} DUPA IN {emotion}, {previous_value}, {percent}")
                 found_zboczes+=1
+                timestamp = frame_idx/frame_rate
             emotions_dict[str(emotion)].append(percent)
             emotions_in_segment = emotions_dict
-            # previous_value = percent
             
         
         if frame_idx%frame_rate==0:
             for key, value in emotions_in_segment.items():
                 mean_emotion_values = np.mean(value)
                 emotion_history.append(mean_emotion_values)
-            # print(frame_idx)
-            # print(emotion_history)
             max_emotion = emotions_labels[np.argmax(emotion_history)]
-            # print(f'For second {frame_idx/frame_rate}, max emotion:{max_emotion}')
             max_emotions_dict[frame_idx/frame_rate] = max_emotion
 
         frame_idx+=1
 
     input_video.release()
     cv2.destroyAllWindows()
+    if found_zboczes<=3:
+        mimika_timestamp = None
+    else:
+        mimika_timestamp=timestamp
 
-    filename = get_filename(file_path)
-    save_emotions_data(filename, emotions_dict)
-    return found_zboczes, max_emotions_dict
+    data = {'mimika_timestamp':mimika_timestamp, 'emotions':max_emotions_dict}
+    with open(json_path, 'w') as file:
+        json.dump(data, file)
+
 
 if __name__=="__main__":
-    videos_path = "/home/probook/Documents/repos/hy_mediape/videos/"
-    # main(f"{videos_path}/HY_2024_film_08.mp4")
-    found_zboczes, max_emotions_dict = main(videos_path, "HY_2024_film_07.mp4")
-    print(found_zboczes)
-    if found_zboczes>3:
-        print("Znaleziono błąd: mimika")
-    print("Emocje:")
-    for keys, values in max_emotions_dict.items():
-        if int(keys)==0:
-            print(f"Sekunda {keys}, emocja {values}")
-            prev_emotion = values
-        else:
-            if values!=prev_emotion:
-                print(f"Sekunda {keys}, emocja {values}")
-                prev_emotion = values
-    # unique_emotions = set(max_emotions_dict.values())
-    # print(unique_emotions)
+    args = parser.parse_args()
 
-    # print(get_filename("videos/HY_2024_film_01.mp4"))
+    f_in = os.path.abspath(args.input)
+    f_out = os.path.abspath(args.output)
+
+    main(f_in, f_out)
+    
+    # print(found_zboczes)
+    # if found_zboczes>3:
+    #     print("Znaleziono błąd: mimika")
+    # print("Emocje:")
+    # for keys, values in max_emotions_dict.items():
+    #     if int(keys)==0:
+    #         print(f"Sekunda {keys}, emocja {values}")
+    #         prev_emotion = values
+    #     else:
+    #         if values!=prev_emotion:
+    #             print(f"Sekunda {keys}, emocja {values}")
+    #             prev_emotion = values
+    # # unique_emotions = set(max_emotions_dict.values())
+    # # print(unique_emotions)
+
+    # # print(get_filename("videos/HY_2024_film_08.mp4"))
+    # # TODO
